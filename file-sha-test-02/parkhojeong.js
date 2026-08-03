77e5c7e302ee731e9ccf81e2164a938fad828fbb
Hojeong Park <parkhj062@gmail.com>
2026-08-04 02:43:54 +0900

    test: add initial file SHA scenarios

---
+++ file-sha-test-02/parkhojeong.js
@@ -0,0 +1,3 @@
+function solution02(numbers) {
+  return numbers.reduce((sum, number) => sum + number, 0);
+}
---
+++ file-sha-test-03/parkhojeong.js
@@ -0,0 +1,3 @@
+function solution03(numbers) {
+  return numbers.reduce((sum, number) => sum + number, 0);
+}
